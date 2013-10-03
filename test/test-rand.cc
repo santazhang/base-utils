0f7821d1ed3f79a9fb19b4b3a5e8aea2a198b62e
Santa Zhang <santa1987@gmail.com>
2013-10-03 14:18:29 -0400

    added benchmark for Rand

---
+++ test/test-rand.cc
@@ -0,0 +1,36 @@
+#include "base/all.h"
+
+using namespace base;
+
+TEST(basetypes, rand_benchmark_single_thread) {
+    Rand r;
+    int n = 100 * 1000 * 1000;
+    Timer t;
+    t.start();
+    for (int i = 0; i < n; i++) {
+        r.next();
+    }
+    t.stop();
+    Log::debug("Rand::next() %d ops in %lf sec (%.2lf nsec each)",
+        n, t.elapsed(), t.elapsed() * 1e9 / n);
+}
+
+TEST(basetypes, rand_benchmark_multi_thread) {
+    Rand r;
+    int n_thread = 10;
+    ThreadPool* tpool = new ThreadPool(n_thread);
+    int n = 10 * 1000 * 1000;
+    Timer t;
+    t.start();
+    for (int j = 0; j < n_thread; j++) {
+        tpool->run_async([&r, n] {
+            for (int i = 0; i < n; i++) {
+                r.next();
+            }
+        });
+    }
+    tpool->release();
+    t.stop();
+    Log::debug("with %d threads, Rand::next() %d ops in %lf sec (%.2lf nsec each)",
+        n_thread, n * n_thread, t.elapsed(), t.elapsed() * 1e9 / n / n_thread);
+}
