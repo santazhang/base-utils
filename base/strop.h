a6d65b0ccb3d7b233698ca1e32f07c7d31089bd6
Santa Zhang <santa1987@gmail.com>
2013-10-11 00:12:56 -0400

    too much parenthese in macros

--- base/strop.h
+++ base/strop.h
@@ -6,7 +6,7 @@
 
 namespace base {
 
-#define streq(a, b) (strcmp((a), (b)) == 0)
+#define streq(a, b) (strcmp(a, b) == 0)
 
 bool startswith(const char* str, const char* head);
 bool endswith(const char* str, const char* head);
