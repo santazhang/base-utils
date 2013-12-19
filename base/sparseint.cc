f3cb60583abe0dafd17b47acba2b542a0742568a
Santa Zhang <santa1987@gmail.com>
2013-12-19 16:28:46 -0500

    finished sparse int

--- base/sparseint.cc
+++ base/sparseint.cc
@@ -73,18 +73,98 @@ int SparseInt::dump(i32 val, char* buf) {
     }
 }
 
-// int SparseInt::dump(i64 i64, char* buf) {
-//     return 0;
-// }
+int SparseInt::dump(i64 val, char* buf) {
+    char* pv = (char *) &val;
+    if (-64 <= val && val <= 63) {
+        buf[0] = pv[0];
+        buf[0] &= 0x7F;
+        return 1;
+    } else if (-8192 <= val && val <= 8191) {
+        buf[0] = pv[1];
+        buf[1] = pv[0];
+        buf[0] &= 0x3F;
+        buf[0] |= 0x80;
+        return 2;
+    } else if (-1048576 <= val && val <= 1048575) {
+        buf[0] = pv[2];
+        buf[1] = pv[1];
+        buf[2] = pv[0];
+        buf[0] &= 0x1F;
+        buf[0] |= 0xC0;
+        return 3;
+    } else if (-134217728 <= val && val <= 134217727) {
+        buf[0] = pv[3];
+        buf[1] = pv[2];
+        buf[2] = pv[1];
+        buf[3] = pv[0];
+        buf[0] &= 0x0F;
+        buf[0] |= 0xE0;
+        return 4;
+    } else if (-17179869184LL <= val && val <= 17179869183LL) {
+        buf[0] = pv[4];
+        buf[1] = pv[3];
+        buf[2] = pv[2];
+        buf[3] = pv[1];
+        buf[4] = pv[0];
+        buf[0] &= 0x07;
+        buf[0] |= 0xF0;
+        return 5;
+    } else if (-2199023255552LL <= val && val <= 2199023255551LL) {
+        buf[0] = pv[5];
+        buf[1] = pv[4];
+        buf[2] = pv[3];
+        buf[3] = pv[2];
+        buf[4] = pv[1];
+        buf[5] = pv[0];
+        buf[0] &= 0x03;
+        buf[0] |= 0xF8;
+        return 6;
+    } else if (-281474976710656LL <= val && val <= 281474976710655LL) {
+        buf[0] = pv[6];
+        buf[1] = pv[5];
+        buf[2] = pv[4];
+        buf[3] = pv[3];
+        buf[4] = pv[2];
+        buf[5] = pv[1];
+        buf[6] = pv[0];
+        buf[0] &= 0x01;
+        buf[0] |= 0xFC;
+        return 7;
+    } else if (-36028797018963968LL <= val && val <= 36028797018963967LL) {
+        buf[1] = pv[7];
+        buf[2] = pv[6];
+        buf[3] = pv[5];
+        buf[4] = pv[4];
+        buf[5] = pv[3];
+        buf[6] = pv[2];
+        buf[7] = pv[1];
+        buf[8] = pv[0];
+        buf[0] = 0xFE;
+        return 8;
+    } else {
+        buf[1] = pv[7];
+        buf[2] = pv[6];
+        buf[3] = pv[5];
+        buf[4] = pv[4];
+        buf[5] = pv[3];
+        buf[6] = pv[2];
+        buf[7] = pv[1];
+        buf[8] = pv[0];
+        buf[0] = 0xFE;
+        buf[0] = 0xFF;
+        return 9;
+    }
+}
+
 
 i32 SparseInt::load_i32(const char* buf) {
     i32 val = 0;
     char* pv = (char *) &val;
     int bsize = SparseInt::buf_size(buf[0]);
-    for (int i = 0; i < bsize; i++) {
-        pv[i] = buf[bsize - i - 1];
-    }
     if (bsize < 5) {
+        for (int i = 0; i < bsize; i++) {
+            pv[i] = buf[bsize - i - 1];
+        }
         pv[bsize - 1] &= 0xFF >> bsize;
         if ((pv[bsize - 1] >> (7 - bsize)) & 0x1) {
             pv[bsize - 1] |= 0xFF << (7 - bsize);
@@ -92,13 +172,35 @@ i32 SparseInt::load_i32(const char* buf) {
                 pv[i] = 0xFF;
             }
         }
+    } else {
+        for (int i = 0; i < 4; i++) {
+            pv[i] = buf[4 - i];
+        }
     }
     return val;
 }
 
-// i64 SparseInt::load_i64(const char* buf) {
-//     i64 val = 0;
-//     return val;
-// }
+i64 SparseInt::load_i64(const char* buf) {
+    i64 val = 0;
+    char* pv = (char *) &val;
+    int bsize = SparseInt::buf_size(buf[0]);
+    if (bsize < 8) {
+        for (int i = 0; i < bsize; i++) {
+            pv[i] = buf[bsize - i - 1];
+        }
+        pv[bsize - 1] &= 0xFF >> bsize;
+        if ((pv[bsize - 1] >> (7 - bsize)) & 0x1) {
+            pv[bsize - 1] |= 0xFF << (7 - bsize);
+            for (int i = bsize; i < 8; i++) {
+                pv[i] = 0xFF;
+            }
+        }
+    } else {
+        for (int i = 0; i < 8; i++) {
+            pv[i] = buf[8 - i];
+        }
+    }
+    return val;
+}
 
 } // namespace base
--- base/sparseint.h
+++ base/sparseint.h
@@ -8,9 +8,9 @@ class SparseInt {
 public:
     static int buf_size(char byte1);
     static int dump(i32 val, char* buf);
-//    static int dump(i64 val, char* buf);
+    static int dump(i64 val, char* buf);
     static i32 load_i32(const char* buf);
-//    static i64 load_i64(const char* buf);
+    static i64 load_i64(const char* buf);
 };
 
 } // namespace base
