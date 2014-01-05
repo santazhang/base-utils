46cb5d1129d9ccb6d5320d7f40186096cb289d64
Santa Zhang <santa1987@gmail.com>
2014-01-05 15:55:39 -0500

    likely/unlikely

--- base/defs.h
+++ base/defs.h
@@ -3,12 +3,20 @@
 #include <stdio.h>
 #include <assert.h>
 
+#ifndef likely
+#define likely(x)   __builtin_expect((x), 1)
+#endif // likely
+
+#ifndef unlikely
+#define unlikely(x)   __builtin_expect((x), 0)
+#endif // unlikely
+
 /**
  * Use assert() when the test is only intended for debugging.
  * Use verify() when the test is crucial for both debug and release binary.
  */
 #ifdef NDEBUG
-#define verify(expr) do { if (!(expr)) { printf("  *** verify failed: %s at %s, line %d\n", #expr, __FILE__, __LINE__); abort(); } } while (0)
+#define verify(expr) do { if (unlikely(!(expr))) { printf("  *** verify failed: %s at %s, line %d\n", #expr, __FILE__, __LINE__); abort(); } } while (0)
 #else
 #define verify(expr) assert(expr)
 #endif
