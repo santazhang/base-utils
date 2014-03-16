59b5c264b62583f39f8a337a0c90b9025f3aab4b
Santa Zhang <santa1987@gmail.com>
2014-03-16 11:25:41 -0400

    minor update

--- base/debugging.cc
+++ base/debugging.cc
@@ -1,4 +1,5 @@
 #include <execinfo.h>
+#include <string.h>
 
 #include "debugging.h"
 
@@ -6,10 +7,13 @@ namespace base {
 
 void __attribute__((noinline)) print_stack_trace(FILE* fp /* =? */) {
     void* callstack[1024];
+    memset(callstack, 0, sizeof(callstack));
     int frames = backtrace(callstack, 1024);
 
+    fprintf(fp, "  *** begin stack trace ***\n");
     // note that we ignore the first frame: print_stack_trace should not be displayed
     backtrace_symbols_fd(callstack + 1, frames - 1, fileno(fp));
+    fprintf(fp, "  ***  end stack trace  ***\n");
 }
 
 } // namespace base
