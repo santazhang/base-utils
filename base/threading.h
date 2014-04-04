9480ae08301a9160c1ab4af75dc60bb6137bf4b7
Santa Zhang <santa1987@gmail.com>
2014-04-04 11:27:16 -0400

    Revert "virtual inheritance"
    This reverts commit adfd760bfc58be1544a8236f25e4aca86488b105.

--- base/threading.h
+++ base/threading.h
@@ -170,7 +170,7 @@ public:
     }
 };
 
-class ThreadPool: virtual public RefCounted {
+class ThreadPool: public RefCounted {
     int n_;
     Counter round_robin_;
     pthread_t* th_;
@@ -190,7 +190,7 @@ public:
     int run_async(const std::function<void()>&);
 };
 
-class RunLater: virtual public RefCounted {
+class RunLater: public RefCounted {
     typedef std::pair<double, std::function<void()>*> job_t;
 
     pthread_t th_;
