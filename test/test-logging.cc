ec557945ed821d0e9ebae3cdcee64731a388a2d0
Santa Zhang <santa1987@gmail.com>
2014-06-04 23:09:20 -0400

    logging-v2: temporarily disable custom streaming

--- test/test-logging.cc
+++ test/test-logging.cc
@@ -17,14 +17,14 @@ ostream& operator <<(ostream& os, const vector<T>& vec) {
 
 TEST(logging, basic_op) {
     vector<int> vec({1, 2, 3});
-    LOG_INFO << "A simple log" << " at INFO level " << vector<int>({1, 2, 3});
+//    LOG_INFO << "A simple log" << " at INFO level " << vector<int>({1, 2, 3});
     LOG_WARN << "A simple log" << " at WARN level";
     LOG_ERROR << "A simple log " << 12345 << " at ERROR level";
     LOG_FATAL << "A simple log " << 'x' << " at FATAL level";
     cout << vec << endl;
     ostringstream ostr;
     ostr << vec << endl;
-    LOG_INFO << vector<int>({1, 2, 3});
+//    LOG_INFO << vector<int>({1, 2, 3});
 
     LOG_INFO("%s ~ %d", "hello", 1987);
     Log_info("%s ~ %d", "hello", 1987);
